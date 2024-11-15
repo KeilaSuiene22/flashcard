25bee749052e6a84bf1fa73dac498d99d4aadf8c
KeilaSuiene22 <keilasuiene@prof.educacao.sp.gov.br>
2024-11-15 07:21:59 -0300

    Flashcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é CSS?',   
+    'A forma completa de CSS é Cascading Style Sheets. É uma linguagem de estilo simples o suficiente para HTML elementos. É popular em web design e sua aplicação também é comum em XHTML.'
+)
+
+criaCartao(
+    'Programação',
+    'O que são estruturas CSS?',
+    'É uma biblioteca pré-planejada, que permite um estilo de página da web mais fácil e compatível com os padrões, usando linguagem CSS.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é HTML5?',
+    'HTML5 é uma linguagem de marcação usada para estruturar e exibir conteúdo na Internet. Isso inclui animações, áudio, imagens e texto, entre muitas outras coisas, e tudo sem a necessidade de software adicional.'
+)
+
+criaCartao(
+    'Programação',
+    'Quais são as 3 tags básicas do HTML?',
+    '    A tag <html> é o elemento raiz que define todo o documento HTML. A tag <head> contém metadados sobre o título e a codificação de caracteres da página. A tag <body> inclui todo o conteúdo que aparece na página.'
+)
